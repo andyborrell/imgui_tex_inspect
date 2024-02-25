9301904e716db9b1918d95063212d1790969a866
Robert Motrogeanu <robertmotrogeanu@yahoo.ca>
2024-02-25 17:44:01 -0500

    added extern declaration

--- imgui_tex_inspect.h
+++ imgui_tex_inspect.h
@@ -2,11 +2,15 @@
 
 #pragma once
 #include "imgui.h"
+#include "imgui_tex_inspect_internal.h"
 
 namespace ImGuiTexInspect
 {
+
 struct Context;
 struct Transform2D;
+
+extern Context *GContext;
 //-------------------------------------------------------------------------
 // [SECTION] INIT & SHUTDOWN
 //-------------------------------------------------------------------------
